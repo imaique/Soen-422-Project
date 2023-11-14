4978723d85400dddc40d9ef2adaefa8630a93b67
Michael <osujim2012@gmail.com>
2023-11-14 01:15:42 -0500

    sync

--- code/api/server.py
+++ code/api/server.py
@@ -1,22 +1,38 @@
 import socket
-import struct
 
 
 def receive_data():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.connect(("192.168.132.73", 1234))
-
-    data_to_send = "Hello from Python!"
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("localhost", 1234))  # Binds to localhost on port 1234
+    server_socket.listen(1)  # Listens for incoming connections
+    print("IP Address: ", get_ip_address())
+    print("Listening on port 1234...")
 
     while True:
-        # Send data to Arduino
-        server_socket.send(data_to_send.encode())
-        print("Data sent to Arduino: {}".format(data_to_send))
-        data, addr = server_socket.recvfrom(1024)
-        print(data.decode())
-        if len(data) == 8:  # Assuming each float is 4 bytes
-            angle, distance = struct.unpack("ff", data)
-            print(f"Received: Angle={angle}, Distance={distance}")
+        (
+            client_socket,
+            client_address,
+        ) = server_socket.accept()  # Accepts incoming connection
+        print(f"Connection established with {client_address}")
+
+        data = client_socket.recv(1024).decode()  # Receives data from the client
+        if not data:
+            break
+
+        try:
+            # Split the received data into two integers
+            [num1, num2] = data
+            print(f"Received integers: {num1} and {num2}")
+        except ValueError:
+            print("Invalid data format. Expected two integers separated by a space.")
+
+        client_socket.close()  # Closes the connection with the client
+
+
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
 
 
 if __name__ == "__main__":
--- code/arduino/sonar/sonar.ino
+++ code/arduino/sonar/sonar.ino
@@ -1,52 +1,100 @@
 #include "WiFi.h"
-#include "AsyncUDP.h"
+#include "WiFiUdp.h"
+#include <ESP32Servo.h>
+
 
 const char * ssid = "MJO";
 const char * password = "michaelosuji";
 
-AsyncUDP udp;
+IPAddress targetAddress = IPAddress(172,25,144,1);      // local port to listen on
+unsigned int targetPort = 1234;      // local port to listen on
 
-void onPacket(AsyncUDPPacket packet) {
-  Serial.print("UDP Packet Type: ");
-  Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
-  Serial.print(", From: ");
-  Serial.print(packet.remoteIP());
-  Serial.print(":");
-  Serial.print(packet.remotePort());
-  Serial.print(", To: ");
-  Serial.print(packet.localIP());
-  Serial.print(":");
-  Serial.print(packet.localPort());
-  Serial.print(", Length: ");
-  Serial.print(packet.length());
-  Serial.print(", Data: ");
-  Serial.write(packet.data(), packet.length());
-  Serial.println();
-  //reply to the client
-  packet.printf("Got %u bytes of data", packet.length());
+WiFiUDP udp;
+Servo myservo;  // create servo object to control a servo
+
+int pos = 16;    // variable to store the servo position
+
+int servoPin = 18;
+
+const int trigPin = 14;
+const int echoPin = 27;
+long duration;
+int distance;
+
+void sendDistance() {
+  // improve distance measurements
+  if(!WiFi.isConnected()) {
+    connectWiFi();
+  }
+
+  udp.beginPacket(targetAddress,targetPort);
+  udp.write(distance);
+  udp.write(pos);
+  udp.endPacket();
+  Serial.println(distance);
 }
 
-void setup()
-{
-    Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("WiFi Failed");
-        while(1) {
-            delay(1000);
-        }
-    }
-    if(udp.listen(1234)) {
-        Serial.print("UDP Listening on IP: ");
-        Serial.println(WiFi.localIP());
-        udp.onPacket(onPacket);
-    }
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
+
+void setupSonar() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+}
+
+void setupServo() {
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectWiFi();
+  setupSonar();
+}
+
+void updateDistance() {
+    // Clears the trigPin
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance based on the speed of sound
+  distance = duration * 0.034 / 2;
+}
+bool shouldSendDistance() {
+  static int lastDistanceSent = 0;
+  static const int distancePeriod = 100;
+
+  const int currentTime = millis();
+  if(currentTime - lastDistanceSent >= distancePeriod) {
+    lastDistanceSent = currentTime;
+    return true;
+  }
+  return false;
 }
 
 void loop()
 {
-    delay(1000);
-    //Send broadcast
-    udp.broadcast("Anyone here?");
+  updateDistance();
+  //Send broadcast every second
+  if(shouldSendDistance()) sendDistance();
 }
