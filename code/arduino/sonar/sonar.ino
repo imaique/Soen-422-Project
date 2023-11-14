a92d388c2e9cad7046a51fc1b8ee2c94e91692e6
Michael <osujim2012@gmail.com>
2023-11-14 01:18:18 -0500

    async stuff

--- code/arduino/sonar/sonar.ino
+++ code/arduino/sonar/sonar.ino
@@ -1,34 +1,43 @@
 #include "WiFi.h"
 #include "AsyncUDP.h"
+#include <ESP32Servo.h>
+
 
 const char * ssid = "MJO";
 const char * password = "michaelosuji";
 
+unsigned int localPort = 1234;      // local port to listen on
+
 AsyncUDP udp;
+Servo myservo;  // create servo object to control a servo
+
+int pos = 16;    // variable to store the servo position
+
+int servoPin = 18;
+
+const int trigPin = 14;
+const int echoPin = 27;
+long duration;
+int distance;
+
+bool badDistance = false;
 
 void onPacket(AsyncUDPPacket packet) {
-  Serial.print("UDP Packet Type: ");
-  Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
-  Serial.print(", From: ");
-  Serial.print(packet.remoteIP());
-  Serial.print(":");
-  Serial.print(packet.remotePort());
-  Serial.print(", To: ");
-  Serial.print(packet.localIP());
-  Serial.print(":");
-  Serial.print(packet.localPort());
-  Serial.print(", Length: ");
-  Serial.print(packet.length());
-  Serial.print(", Data: ");
+  // improve distance measurements
+  badDistance = true;
+  Serial.print("Data: ");
   Serial.write(packet.data(), packet.length());
   Serial.println();
   //reply to the client
-  packet.printf("Got %u bytes of data", packet.length());
+  AsyncUDPMessage udpMessage(8);
+  udpMessage.write(distance);
+  udpMessage.write(pos);
+  Serial.println(distance);
+  packet.send(udpMessage);
+  //packet.printf("Got %u bytes of data", packet.length());
 }
 
-void setup()
-{
-    Serial.begin(115200);
+void setupWifi() {
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -37,16 +46,49 @@ void setup()
             delay(1000);
         }
     }
-    if(udp.listen(1234)) {
-        Serial.print("UDP Listening on IP: ");
-        Serial.println(WiFi.localIP());
-        udp.onPacket(onPacket);
-    }
+}
+
+void setupSonar() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+}
+
+void setupServo() {
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupWifi();
+  setupSonar();
+}
+
+void updateDistance() {
+    // Clears the trigPin
+  badDistance = false;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance based on the speed of sound
+  const int calculatedDistance = duration * 0.034 / 2;
+  if(badDistance) return;
+  distance = calculatedDistance;
 }
 
 void loop()
 {
-    delay(1000);
-    //Send broadcast
-    udp.broadcast("Anyone here?");
+  updateDistance();
+  //Send broadcast every second
+  if(!(millis() % 1000)) udp.broadcast("Anyone here?");
 }
